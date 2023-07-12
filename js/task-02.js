280f73f46b615af5d51612aeb14a169c903584d5
a7ndriyyy <andrii.nastych@gmail.com>
2023-07-12 14:57:43 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector("#ingredients");
+const createEl = ingredients.map((ingredients) => {
+const li = document.createElement("li");
+li.textContent = ingredients;
+li.classList.add("item");
+return li ;
+});
+console.log(createEl);
+ingredientsListEl.append(...createEl);
