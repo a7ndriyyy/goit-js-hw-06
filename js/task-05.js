4f52504d0221a83e6d6149359ac80fa0f05dcc18
a7ndriyyy <andrii.nastych@gmail.com>
2023-07-12 15:43:52 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const spanEl = document.querySelector("#name-output");
 inputValue.addEventListener("input", onInput);
 
 function onInput(peyn) {
-    if (peyn.currentTarget.value.trim() === "") {
+if (peyn.currentTarget.value.trim() === "") {
         spanEl.textContent = "Anonymous";
     } else {
         spanEl.textContent = peyn.currentTarget.value;
